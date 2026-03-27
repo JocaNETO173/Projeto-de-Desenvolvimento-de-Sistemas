74942173956057b0519b6d1f77bf190658b4db2d
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-03-27 11:29:28 -0300

    Delete ProjetosPython/Jogos directory

--- ProjetosPython/Jogos/For and While/multiplicacao.py
+++
@@ -1,7 +0,0 @@
-num = int(input('Escolha um número\n'))
-i=1
-
-
-for i in range(1, 11):
-    multi = num * i
-    print(f'{num} x {i} = {multi}')
--- ProjetosPython/Jogos/teste.py
+++
@@ -1,3 +0,0 @@
-palavra = 'pato'
-
-print(palavra.strip())
