2f08a9ab77e418ebbee53b17ca1aad49a38a4692
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-03-27 11:29:18 -0300

    Add files via upload

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,12 +1,26 @@
+import time
+
 def jogar_forca():
     print('*--------------------*')
     print('BEM VINDO AO JOGO DE FORCA')
     print('*--------------------*')
-    palavra_secreta = "carambóla"
+    print('Escolhendo Palavra')
+    time.sleep(1)
+    print('.')
+    time.sleep(1)
+    print('..')
+    time.sleep(1)
+    print('...')
+    time.sleep(1)
+    print('Palavra escolhida!')
+    time.sleep(1)
+    palavra_secreta = "carambola"
+    letras_acertadas = ["_","_","_","_","_","_","_","_","_"]
     perdeu = False
     acertou = False
 
     while(not perdeu and not acertou):
+        print(f'Palavra: {letras_acertadas}')
         chute = input('Digite uma Letra: ')
         chute = chute.strip()
 
@@ -15,7 +29,8 @@ def jogar_forca():
 
         for letra in palavra_secreta:
             if(chute.lower() == letra.lower()):
-                print(f'A letra {chute} está na posição {index}!')
+                # print(f'A letra {chute} está na posição {index}!')
+                letras_acertadas[index] = letra
             index = index+1
 
 
--- Jogos/teste.py
+++ Jogos/teste.py
@@ -1,13 +1,24 @@
-palavra = 'carambola'
+# palavra = 'carambola'
 
-teste_valores = []
-print(type(teste_valores))
+# teste_valores = []
+# print(type(teste_valores))
 
-i = 0
-secreto = ''
+# i = 0
+# secreto = ''
 
-for i in range(len(palavra)):
-    teste_valores.append('_')
-    secreto += f'{teste_valores[i]} '
-print(secreto)
+# for i in range(len(palavra)):
+#     teste_valores.append('_')
+#     secreto += f'{teste_valores[i]} '
+# print(secreto)
 
+# tupla = (1,2,3,4)
+# tupla.index
+# print(tupla)
+
+# p1 = (2,3)
+# p2 = (6,8)
+# p3 = (10,12)
+
+# linha = [p1,p2,p3]
+
+# print(linha[1][1])
