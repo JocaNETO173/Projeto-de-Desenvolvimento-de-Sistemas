48a63ba04a6def350c2599adb10f11f612cab5f6
JocaNETO173 <joaquim.custodio.neto@escola.pr.gov.br>
2026-03-30 11:50:02 -0300

    Add files via upload

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,4 +1,5 @@
 import time
+import random as rd
 
 def jogar_forca():
     print('*--------------------*')
@@ -11,27 +12,56 @@ def jogar_forca():
     print('..')
     time.sleep(1)
     print('...')
-    time.sleep(1)
-    print('Palavra escolhida!')
-    time.sleep(1)
-    palavra_secreta = "carambola"
-    letras_acertadas = ["_","_","_","_","_","_","_","_","_"]
+
+    arquivo = open("palavras.txt" , "r")
+    palavras = []
+    
+    for linha in palavras:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+    palavra_escolhida = int(rd.randint(0, len(palavras)))
+
+    # time.sleep(1)
+    # print('Palavra escolhida!')
+    # time.sleep(1)
+    palavra_secreta = palavras[palavra_escolhida]
+    letras_acertadas = ["_" for letra in palavra_secreta]
     perdeu = False
     acertou = False
+    erros = 0
 
+    print(f'Palavra: {letras_acertadas}')
     while(not perdeu and not acertou):
-        print(f'Palavra: {letras_acertadas}')
+        
         chute = input('Digite uma Letra: ')
-        chute = chute.strip()
+        chute = chute.strip().lower()
 
         #Index define a posição da letra
-        index = 0
+        
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.lower() == letra.lower()):
+                    # print(f'A letra {chute} está na posição {index}!')
+                    letras_acertadas[index] = letra
+                index += 1
+                
+        else:
+            erros = erros + 1
+            
+        perdeu = erros == 6
+        acertou = "_" not in letras_acertadas
 
-        for letra in palavra_secreta:
-            if(chute.lower() == letra.lower()):
-                # print(f'A letra {chute} está na posição {index}!')
-                letras_acertadas[index] = letra
-            index = index+1
+        
+        print(f'Palavra: {letras_acertadas}')
+        print(f'Erros: {erros}')
+        if perdeu == True:
+            print('Você perdeu!')
+        elif acertou == True:
+            print('Parabéns, você adivinhou a palavra secreta!')
+        
 
 
 
